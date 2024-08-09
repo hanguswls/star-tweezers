3127d4fb8a12a7678c6135d47d0beb437a6deb98
Jonghyeok <jhpmuscle@gmail.com>
2024-08-09 15:03:27 +0900

    initial setting

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { BrowserRouter } from 'react-router-dom'
+import { CookiesProvider } from 'react-cookie'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <CookiesProvider>
+    <BrowserRouter>
+      <StrictMode>
+        <App />
+      </StrictMode>,
+    </BrowserRouter>
+  </CookiesProvider>
 )
