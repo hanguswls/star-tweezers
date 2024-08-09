7fe2777f441d8b401dd9d831616ab830d5bd70e1
Jonghyeok <jhpmuscle@gmail.com>
2024-08-09 16:08:04 +0900

    style: 기본 배치 세팅

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import { Route, Routes } from 'react-router-dom'
 
@@ -9,10 +7,12 @@ function App() {
 
   return (
     <>
-      <Routes>
-        <Route path="/" element="" />
-        <Route path="*" element="페이지를 찾을 수 없습니다." />
-      </Routes>
+      <main className="mobile-wrapper">
+        <Routes>
+          <Route path="/" element="" />
+          <Route path="*" element="페이지를 찾을 수 없습니다." />
+        </Routes>
+      </main>
     </>
   )
 }
