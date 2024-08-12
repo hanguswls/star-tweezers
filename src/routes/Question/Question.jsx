be20991fd96c9740d7a4f56ceb9ff430cb98e650
hj <hanguswls@knu.ac.kr>
2024-08-12 23:35:53 +0900

    refactor: 헤드라인과 문제를 분리

--- src/routes/Question/Question.jsx
+++ src/routes/Question/Question.jsx
@@ -1,40 +1,19 @@
 import './Question.css';
 import useQuestion from '../../hooks/useQuestion';
 import QuestionItem from '../../components/QuestionITem/QuestionItem';
+import Headline from '../../components/Headline/Headline'
+import useHeadline from '../../hooks/useHeadline';
 
 function Question() {
-  const {
-    question,
-    titleRef,
-    descriptionRef,
-    handleTitleChange,
-    handleDescriptionChange,
-    handleUpdateTitle,
-    handleUpdateDescription
-  } = useQuestion();
+  const {headline} = useHeadline();
+  const {questions} = useQuestion();
 
   return (
     <main className='question'>
-      <section className='question-header'>
-        <p className='created-at'>{question.createdAt}</p>
-        <textarea
-          ref={titleRef}
-          className='header-title'
-          value={question.title}
-          onChange={(e)=>handleTitleChange(e.target.value)}
-          onBlur={(e)=>handleUpdateTitle(e.target.value)}
-        />
-        <textarea
-          ref={descriptionRef}
-          className='header-description'
-          value={question.description}
-          onChange={(e)=>handleDescriptionChange(e.target.value)}
-          onBlur={(e)=>handleUpdateDescription(e.target.value)}
-        />
-      </section>
+      <Headline headline={headline}/>
       <hr />
       <section className='question-content-container'>
-        {question.contents.map((q) => (
+        {questions.map((q) => (
           <QuestionItem
             key={q.id}
             id={q.id}
