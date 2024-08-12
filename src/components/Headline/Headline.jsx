ceb54c22db002709567fe9bad08c7af365098fb4
hj <hanguswls@knu.ac.kr>
2024-08-13 01:36:57 +0900

    style: 헤드라인 레이아웃 및 스타일링

---
+++ src/components/Headline/Headline.jsx
@@ -0,0 +1,36 @@
+import './Headline.css';
+import useHeadline from "../../hooks/useHeadline"
+
+function Headline() {
+  const {
+    headline,
+    titleRef,
+    descriptionRef,
+    handleTitleChange,
+    handleDescriptionChange,
+    handleUpdateTitle,
+    handleUpdateDescription
+  } = useHeadline();
+
+  return (
+    <section className='headline'>
+      <p className='created-at'>{headline.createdAt}</p>
+    <textarea
+      ref={titleRef}
+      className='headline-title'
+      value={headline.title}
+      onChange={(e)=>handleTitleChange(e.target.value)}
+      onBlur={(e)=>handleUpdateTitle(e.target.value)}
+    />
+    <textarea
+      ref={descriptionRef}
+      className='headline-description'
+      value={headline.description}
+      onChange={(e)=>handleDescriptionChange(e.target.value)}
+      onBlur={(e)=>handleUpdateDescription(e.target.value)}
+    />
+  </section>
+  )
+}
+
+export default Headline;
